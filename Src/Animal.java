6c29f337da4c9ed4e8d4235fc7d972819387b0b4
Maite <mariateresa2305@gmail.com>
2021-09-15 20:04:28 -0500

    Archivos sobre practica de POO

---
+++ Src/Animal.java
@@ -0,0 +1,16 @@
+package Src;
+
+public class Animal {
+    public String breed;
+    public String color;
+    public String name;
+    public boolean move(int distanceX, int distanciaY) {
+        return true;
+    }
+    public void sleep() {
+        System.out.println("Estoy durmiendo");
+    }
+    public void eat() {
+        System.out.println("Estoy comiendo");
+    }
+}
---
+++ Src/MainClass.java
@@ -0,0 +1,24 @@
+package Src;
+
+import java.util.Date;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Item tuna = new Item();
+        tuna.name = "Atun Gomez";
+        tuna.price = 180.0;
+        tuna.expirationDate = new Date(2021,8,12
+        );
+        tuna.calculateTax();
+
+        if (tuna.canItBeSold()) {
+
+            System.out.println("Nombre del Item: " + tuna.name);
+            System.out.println("Precio del Item: " + tuna.price);
+            System.out.println("Impuesto del Item: " + tuna.tax);
+            System.out.println("Total a pagar por el Item: " + tuna.getTotalToPay());
+        } else {
+            System.out.println("Este Item no esta habilitado para la venta");
+        }
+    }
+}
